a605699d46f361bde289204e61582cdfc25ed16c
mdshamoon <mohdshamoon841@gmail.com>
2023-09-01 21:16:19 +0530

    changed base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://dalgo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/dalgo_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
