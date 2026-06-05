054dbc8e6c5f96321e7947b3e2fb3a123f42c2cb
Ishankoradia <ikoradia@umich.edu>
2026-06-05 14:51:19 +0530

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,10 +19,9 @@ const config = {
     favicon: "img/favicon_new.svg",
 
     // Set the production url of your site here
-    url: "https://dalgo.github.io",
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/dalgo_docs/",
+    url: "https://docs.dalgo.org",
+    // Served at the root of the custom domain
+    baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
